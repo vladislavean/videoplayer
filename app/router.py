4249aafbfc215e875a2f06bbb730e6ee21a609db
Jorique <guselnikovgeorgiy@gmail.com>
2024-09-24 20:50:39 +0300

    added get endpoints, get_video endpoint

--- app/router.py
+++ app/router.py
@@ -1,7 +1,11 @@
+import uuid
+from fastapi import HTTPException
+
 from fastapi import APIRouter
 
-from app.database.db import select_all
-from app.database.models import Streets
+from app.database.db import select_all, find_all, find_one_or_none
+from app.database.models import Streets, Cameras, ArchivesTask
+from app.utils import download_video
 
 router = APIRouter()
 
@@ -13,6 +17,27 @@ async def get_streets():
 
 @router.get("/cameras")
 async def get_cameras():
-    pass
+    return await select_all(Cameras)
 
 
+@router.get("/archives")
+async def get_archives():
+    return await select_all(ArchivesTask)
+
+
+@router.get("/cameras/{street_id}")
+async def get_cameras_by_street(street_id: uuid.UUID):
+    return await find_all(Cameras, streetId=street_id)
+
+
+@router.get("/archives/{camera_id}")
+async def get_archives_by_camera(camera_id: uuid.UUID):
+    return await find_all(ArchivesTask, cameraId=camera_id)
+
+
+@router.get("/archive/{archive_id}")
+async def get_archive_video(archive_id: uuid.UUID):
+    archive = await find_one_or_none(ArchivesTask, id=archive_id)
+    if archive is None:
+        raise HTTPException(status_code=404)
+    return await download_video(archive.url)
