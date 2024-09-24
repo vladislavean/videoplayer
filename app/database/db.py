a52026af91890e9926c424d98d8bc30711badd31
Jorique <guselnikovgeorgiy@gmail.com>
2024-09-24 19:21:41 +0300

    added select options

--- app/database/db.py
+++ app/database/db.py
@@ -1,6 +1,30 @@
-from sqlalchemy.orm import DeclarativeBase
 from app.database.settings import postgre_settings
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from sqlalchemy import select, insert, update, delete
 
 
-class Base(DeclarativeBase):
-    pass
+engine = create_async_engine(postgre_settings.DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def select_all(model):
+    async with async_session_maker() as session:
+        query = select(model.__table__.columns)
+        result = await session.execute(query)
+        return result.mappings().all()
+
+
+async def find_one_or_none(model, **filter_by):
+    async with async_session_maker() as session:
+        query = select(model.__table__.columns).filter_by(**filter_by)
+        result = await session.execute(query)
+        return result.mappings().one_or_none()
+
+
+async def find_all(model, **filter_by):
+    async with async_session_maker() as session:
+        query = select(model.__table__.columns).filter_by(**filter_by)
+        result = await session.execute(query)
+        return result.mappings().all()
+
