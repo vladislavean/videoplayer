db3c981ab9eebe1c71985b771fd4e34379aa8161
Jorique <guselnikovgeorgiy@gmail.com>
2024-09-24 20:51:08 +0300

    added download_video util

--- app/utils.py
+++ app/utils.py
@@ -1,12 +1,26 @@
-from app.storage import NextCLoudSettings
+# from app.storage import NextCLoudSettings
+#
+#
+# class StorageServer:
+#
+#     @classmethod
+#     def create_file_info(cls):
+#         ...
+#
+#     @classmethod
+#     def upload_file_to_server_by_ssh(cls):
+#         ...
+import requests
+from io import BytesIO
+from fastapi.responses import StreamingResponse
+from fastapi import HTTPException
 
 
-class StorageServer:
+async def download_video(url):
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
 
-    @classmethod
-    def create_file_info(cls):
-        ...
-
-    @classmethod
-    def upload_file_to_server_by_ssh(cls):
-        ...
+        return StreamingResponse(BytesIO(response.content), media_type="video/mp4")
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail=f"Error downloading video: {url}. {str(e)}")
