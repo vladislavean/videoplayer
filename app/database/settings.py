78af9b3877b3abdac5f1114214f500bf430b11b6
Jorique <guselnikovgeorgiy@gmail.com>
2024-09-24 19:22:26 +0300

    moved session to db.py

--- app/database/settings.py
+++ app/database/settings.py
@@ -1,6 +1,4 @@
-from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 
 class PostgreSettings(BaseSettings):
@@ -13,11 +11,6 @@ class PostgreSettings(BaseSettings):
     @property
     def DATABASE_URL(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
-    @property
-    def ASYNC_SESSIONMAKER_OBJECT(self):
-        engine = create_async_engine(postgre_settings.DATABASE_URL)
-        return async_sessionmaker(engine, expire_on_commit=False)
 
     model_config = SettingsConfigDict(env_file=".env")
 
