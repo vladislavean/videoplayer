aee8ea64a058def97ecee110e97558ff4958b5ca
vladislavean <vladrollss228@gmail.com>
2024-09-25 14:35:41 +0300

    storage settings

--- app/storage/settings.py
+++ app/storage/settings.py
@@ -2,4 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class StorageSettings(BaseSettings):
-    ...
+
+    SSH_NAME: str
+    SSH_PASS: str
+
+    model_config = SettingsConfigDict(env_file=".env")
