36eea2adfa490034de055fc626a28a6279277d4a
vladislavean <vladrollss228@gmail.com>
2024-09-25 15:41:58 +0300

    storage create_street_dir

--- app/storage/storage_utils.py
+++ app/storage/storage_utils.py
@@ -21,17 +21,29 @@ class StorageSSH:
         except asyncssh.connection.PermissionDenied as e:
             raise HTTPException(status_code=403, detail=f"Permission Denied. {e}")
 
+    async def create_street_folder_or_ignore(
+            self,
+            street_id: uuid.UUID
+    ) -> None | str:
+        connection = await self._connection_by_ssh()
+        async with connection.start_sftp_client() as sftp_client:
+            if not await sftp_client.exists(f"/files/{street_id}/"):
+                await sftp_client.makedirs(f"/files/{street_id}")
+            else:
+                return None
+
     async def upload_archive(
             self,
             archive_id: uuid.UUID,
             archive_in_bytes: bytes,
-            street_id: str
+            camera_title: str,
+            street_id: uuid.UUID
     ):
         async with tempfile.NamedTemporaryFile(delete=False) as temp_file:
             temp_file.write(archive_in_bytes)
             temp_file_path = temp_file.name
 
         connection = await self._connection_by_ssh()
-        async with connection.start_sftp() as sftp_client:
-            await sftp_client.put(temp_file_path, f'/files/{street_id}/{archive_id}.mp4')
+        async with connection.start_sftp_client() as sftp_client:
+            await sftp_client.put(temp_file_path, f'/files/{str(street_id)}/{camera_title}/{archive_id}.mp4')
 
