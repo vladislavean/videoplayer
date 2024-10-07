520f62a63902757d1b97529221bb4ae29e4d817e
vladislavean <vladrollss228@gmail.com>
2024-10-07 22:20:37 +0300

    add corsm

--- app/main.py
+++ app/main.py
@@ -2,22 +2,16 @@ from fastapi import FastAPI
 from app.router import router as api_router
 from fastapi.middleware.cors import CORSMiddleware
 
-
 app = FastAPI()
 app.include_router(api_router)
-origins = [
 
-    "http://10.8.0.6:3000/videoCamers",
-]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
-    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
-                   "Access-Control-Allow-Origin", "Authorization"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
 )
-
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="10.8.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
