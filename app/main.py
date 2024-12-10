d9d5285b7a20ff23ce841c7291efcd782836f599
Jorique <guselnikovgeorgiy@gmail.com>
2024-12-11 02:34:10 +0300

    added redis events

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+
+from app.database.redis_settings import redis
 from app.router import router as api_router
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -12,6 +14,22 @@ app.add_middleware(
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
+
+
+@app.on_event("startup")
+async def startup():
+    try:
+        await redis.ping()
+        print("Redis connection established")
+    except Exception as e:
+        print(f"Redis connection failed: {e}")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await redis.close()
+
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
