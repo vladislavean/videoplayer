9d68027c907b3de9ee3769297b2e9257ec25e818
Jorique <guselnikovgeorgiy@gmail.com>
2024-12-16 01:37:41 +0300

    added exist conditions

--- app/routers/admin/streets.py
+++ app/routers/admin/streets.py
@@ -1,8 +1,8 @@
 import uuid
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
-from app.database.db import get_async_session, insert_one, update_one, delete_one
+from app.database.db import get_async_session, insert_one, update_one, delete_one, find_one_or_none
 from app.database.models import Streets
 from app.dependencies import get_auth_admin
 
@@ -17,12 +17,18 @@ admin_streets_router = APIRouter(
 @admin_streets_router.post("/add", summary="Создание новой улицы")
 async def add_street(street_name: str):
     async with get_async_session() as session:
+        street = await find_one_or_none(session=session, model=Streets, name=street_name)
+        if street:
+            raise HTTPException(status_code=400, detail="Улица с таким названием уже существует")
         return await insert_one(session=session, model=Streets, name=street_name)
 
 
 @admin_streets_router.put("/update/{id}", summary="Обновление улицы")
 async def update_street(id: uuid.UUID, name: str):
     async with get_async_session() as session:
+        street = await find_one_or_none(session=session, model=Streets, name=name)
+        if street.id != id:
+            raise HTTPException(status_code=400, detail="Улица с таким названием уже существует")
         return await update_one(session=session, model=Streets, id=id, name=name)
 
 
