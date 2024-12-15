9cf1deca4880114f55139e84c4efdc4d805486b6
Jorique <guselnikovgeorgiy@gmail.com>
2024-12-15 23:54:44 +0300

    added get_auth_user & fixed status code

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,32 +1,42 @@
-from fastapi import Request, Depends, Response, status
+from fastapi import Request, Depends, HTTPException
 from app.database.db import get_async_session, find_one_or_none
 from app.database.models import Users, FunctionalRoles
 
 from app.database.redis_settings import redis
 
 
-async def get_current_user(request: Request):
-    session = request.cookies.get("session_id")
+async def get_current_user(request: Request) -> str:
+    session = str(request.cookies.get("session_id"))
     if not session:
-        return None
-    user_id = await redis.exists(session)
+        raise HTTPException(status_code=401, detail="Вы не авторизованы")
+    user_id = await redis.get(session)
     if not user_id:
-        return None
+        raise HTTPException(status_code=401, detail="Вы не авторизованы")
     return user_id
 
 
-async def get_auth_admin(user_id = Depends(get_current_user)):
+async def get_auth_user(user_id: str = Depends(get_current_user)) -> Users:
     if not user_id:
-        return status.HTTP_401_UNAUTHORIZED(detail="Вы не авторизованы")
+        raise HTTPException(status_code=401, detail="Вы не авторизованы")
+    async with get_async_session() as session:
+        user = await find_one_or_none(session=session, model=Users, id=str(user_id))
+        if not user:
+            raise HTTPException(status_code=401, detail="Вы не авторизованы")
+        return user
+
+
+async def get_auth_admin(user_id: str = Depends(get_current_user)) -> Users:
+    if not user_id:
+        raise HTTPException(status_code=401, detail="Вы не авторизованы")
     async with get_async_session() as session:
         user = await find_one_or_none(session=session, model=Users, id=user_id)
         if not user:
-            return status.HTTP_401_UNAUTHORIZED(detail="Вы не авторизованы")
+            raise HTTPException(status_code=401, detail="Вы не авторизованы")
         role = await find_one_or_none(session=session, model=FunctionalRoles, id=user.roleId)
         if not role:
-            return status.HTTP_401_UNAUTHORIZED(detail="Вы не авторизованы")
+            raise HTTPException(status_code=401, detail="Вы не авторизованы")
         if role.name != "admin":
-            return status.HTTP_401_UNAUTHORIZED(detail="Вы не авторизованы")
+            raise HTTPException(status_code=401, detail="У вас нет доступа")
         return user
 
 
