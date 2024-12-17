3082468bbe59dea368cfb193e75d2b5a60f07963
Jorique <guselnikovgeorgiy@gmail.com>
2024-12-17 21:00:53 +0300

    debug

--- app/main.py
+++ app/main.py
@@ -44,7 +44,7 @@ app.include_router(admin_roles_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="http://10.8.0.1:3000",
+    allow_origins="http://localhost:3000",
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
