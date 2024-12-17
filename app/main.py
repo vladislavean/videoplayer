d075aca082462f3c84a93021dd8faa01a4b37de5
Jorique <guselnikovgeorgiy@gmail.com>
2024-12-17 18:04:08 +0300

    cors

--- app/main.py
+++ app/main.py
@@ -47,13 +47,7 @@ app.add_middleware(
     allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=[
-        "Content-Type",
-        "Set-Cookie",
-        "Access-Control-Allow-Headers",
-        "Access-Control-Allow-Origin",
-        "Access-Authorization",
-    ],
+    allow_headers=["*"],
 )
 
 
